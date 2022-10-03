48168ee30341bdf2629862ab4c3e3019392256ef
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2022-10-03 00:59:40 -0500

    completed section navigation using html

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('scroll', function (e) {
+    var i = 0;
+    var sectionElements = document.getElementsByTagName("section");
+    while (i < sectionElements.length) {
+        var section = sectionElements[i];
+        if (section.getBoundingClientRect().top >= 0) {
+            break;
+        }
+        i++;
+    }
+    // @ts-ignore
+    history.replaceState(null, null, '#' + sectionElements[i].id);
+    var navLinks = document.getElementsByTagName('a');
+    for (var i_1 = 0; i_1 < navLinks.length; i_1++) {
+        if (navLinks[i_1].hash == window.location.hash) {
+            navLinks[i_1].classList.add("active");
+        }
+        else {
+            navLinks[i_1].classList.remove("active");
+        }
+    }
+});
---
+++ index.ts
@@ -0,0 +1,26 @@
+document.addEventListener('scroll', (e: Event) => {
+    let i = 0;
+    let sectionElements: HTMLCollectionOf<HTMLElement> = document.getElementsByTagName("section")
+
+    while (i < sectionElements.length) {
+        let section = sectionElements[i]
+        if (section.getBoundingClientRect().top >= 0) {
+            break;
+        }
+        i++;
+    }
+
+    if (window.location.hash != '#' + sectionElements[i].id) {
+        // @ts-ignore
+        history.replaceState(null, null, '#' + sectionElements[i].id);
+    }
+
+    let navLinks: HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName('a')
+    for (let i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].hash == window.location.hash) {
+            navLinks[i].classList.add("active");
+        } else {
+            navLinks[i].classList.remove("active");
+        }
+    }
+})
